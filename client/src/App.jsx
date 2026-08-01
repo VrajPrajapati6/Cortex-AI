f5021dc22f61582ac8e74376c07698779c5d10c8
prince-patel-cse <prince.patel.cse@gmail.com>
2026-08-01 17:35:14 +0530

    Fixed duplicate TopologyGraph import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,6 @@ import { IncidentsList } from './components/IncidentsList';
 import { IncidentRcaModal } from './components/IncidentRcaModal';
 import ServiceHealthMap from './components/ServiceHealthMap';
 import TopologyGraph from './components/TopologyGraph';
-import TopologyGraph from './components/TopologyGraph';
 
 const socket = io('http://localhost:5000');
 
